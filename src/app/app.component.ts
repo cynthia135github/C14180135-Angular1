3ff42c6959d835cf185ea8f86d5131e3fb9a3e4a
cynthia135github <c14180135@john.petra.ac.id>
2021-02-25 17:02:01 +0700

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,109 +1,126 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
 
-  output : String[][];
+  output: String[][];
 
+  winner = 0;
   flagpemain = 1;
   baris = 0;
   kolom = 0;
 
-  constructor(){
+  player1 = 0;
+  player2 = 0;
+
+  constructor() {
     this.output = [];
 
-    for(var brs: number = 0; brs < 5; brs++) {
-        this.output[brs] = [];
-        for(var klm: number = 0; klm< 5; klm++) {
-            this.output[brs][klm] = "*";
-        }
+    for (var brs: number = 0; brs < 5; brs++) {
+      this.output[brs] = [];
+      for (var klm: number = 0; klm < 5; klm++) {
+        this.output[brs][klm] = "*";
+      }
     }
   }
 
-  InputPlayer(){
+  InputPlayer() {
     //alert(this.output[0][1]);
-    if(this.baris > 0){
-       this.baris -= 1;
+    if (this.baris > 0) {
+      this.baris -= 1;
     }
-    if(this.kolom > 0){
-       this.kolom -= 1;
+    if (this.kolom > 0) {
+      this.kolom -= 1;
     }
-    
-    if(this.flagpemain == 1){
-      if(this.output[this.baris][this.kolom] == "*"){
-          this.output[this.baris][this.kolom] = "1";
-          var hsl = this.cekMenang()
-          this.flagpemain = 2;
-          this.baris = 0;
-          this.kolom = 0;
-          if(hsl == 1){
-            alert("Player 1 Menang !!!")
-            this.reset();
-          }
+
+    if (this.flagpemain == 1) {
+      if (this.output[this.baris][this.kolom] == "*") {
+        this.output[this.baris][this.kolom] = "1";
+        var hsl = this.cekMenang();
+        this.flagpemain = 2;
+        this.baris = 0;
+        this.kolom = 0;
+        if (hsl == 1) {
+          this.winner = 1;
+          //alert("Player 1 Menang !!!");
+          //this.reset();
+        }
+      } else {
+        alert("Silahkan Pilih Baris dan Kolom yg lain !");
+        this.baris = 0;
+        this.kolom = 0;
       }
-      else{
-        alert("Silahkan Pilih Baris dan Kolom yg lain !")
+    } else {
+      if (this.output[this.baris][this.kolom] == "*") {
+        this.output[this.baris][this.kolom] = "2";
+        var hsl = this.cekMenang();
+        this.flagpemain = 1;
+        this.baris = 0;
+        this.kolom = 0;
+        if (hsl == 2) {
+          this.winner = 2;
+          //alert("Player 2 Menang !!!");
+          //this.reset();
+        }
+      } else {
+        alert("Silahkan Pilih Baris dan Kolom yg lain !");
+        this.baris = 0;
+        this.kolom = 0;
       }
-      
     }
-    else{
-       if(this.output[this.baris][this.kolom] == "*"){
-          this.output[this.baris][this.kolom] = "2";
-          var hsl = this.cekMenang()
-          this.flagpemain = 1;
-          this.baris = 0;
-          this.kolom = 0;
-          if(hsl == 2){
-            alert("Player 2 Menang !!!")
-            this.reset();
-          }
-      }
-      else{
-        alert("Silahkan Pilih Baris dan Kolom yg lain !")
-      }
-    }
-  } 
-  
-  reset(){
+  }
+
+  reset() {
     this.flagpemain = 1;
+    this.winner = 0;
     this.baris = 0;
     this.kolom = 0;
     this.output = [];
 
-    for(var brs: number = 0; brs < 5; brs++) {
-        this.output[brs] = [];
-        for(var klm: number = 0; klm< 5; klm++) {
-            this.output[brs][klm] = "*";
-        }
-    }
-
-   
-  }
-
-  cekMenang(){
-    for(var b:number, b=0; b<5; b++){
-      for(var k:number, k=0; k<5; k++){
-        if(this.output[b][0] == "1" && this.output[b][1] == "1" && this.output[b][2] == "1" && this.output[b][3] == "1" &&this.output[b][4] == "1" ){
-              return 1;
-        }
-        if(this.output[0][k] == "1" && this.output[1][k] == "1" && this.output[2][k] == "1" && this.output[3][k] == "1" &&this.output[4][k] == "1" ){
-              return 1;
-        }
-        if(this.output[b][0] == "2" && this.output[b][1] == "2" && this.output[b][2] == "2" && this.output[b][3] == "2" &&this.output[b][4] == "2" ){
-              return 2;
-        }
-        if(this.output[0][k] == "2" && this.output[1][k] == "2" && this.output[2][k] == "2" && this.output[3][k] == "2" &&this.output[4][k] == "2" ){
-              return 2;
-        }
-
+    for (var brs: number = 0; brs < 5; brs++) {
+      this.output[brs] = [];
+      for (var klm: number = 0; klm < 5; klm++) {
+        this.output[brs][klm] = "*";
       }
     }
-  
   }
-  
+
+  cekMenang() {
+    var charnow = this.flagpemain.toString();
+    var jum = 0;
+
+    // cek horizontal
+    var k = 0;
+    while (this.output[this.baris][k] != charnow) {
+      k++;
+    }
+    while (k < 5 && this.output[this.baris][k] == charnow) {
+      k++;
+      jum++;
+    }
+
+    if (jum >= 4) {
+      return this.flagpemain;
+    }
+
+    //cek vertical
+    var jumVertical = 0;
+    var i = 0;
+    while (this.output[i][this.kolom] != charnow) {
+      i++;
+    }
+    while (i < 5 && this.output[i][this.kolom] == charnow) {
+      i++;
+      jumVertical++;
+    }
+
+    if (jumVertical >= 4) {
+      return this.flagpemain;
+    }
+  }
 }
